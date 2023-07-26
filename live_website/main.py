abfc4b6f3bc332632046a2772f277ad628ef134a
Rokon Ron <rokon.raz@gmail.com>
2023-07-27 00:07:01 +0600

    Here print all link and all 'a' text.

--- live_website/main.py
+++ live_website/main.py
@@ -5,7 +5,8 @@ response = requests.get("https://news.ycombinator.com/news")
 lv_web_page = response.text
 
 soup = BeautifulSoup(lv_web_page, "html.parser")
-article_tag = soup.find(name='a', rel="noreferrer")
+
+# article_tag = soup.find(name='a', rel="noreferrer")
 
 # for tag in all_tag_find:
 #     all_tags_find_all = tag.getText()
@@ -17,8 +18,27 @@ article_tag = soup.find(name='a', rel="noreferrer")
 # tag_find = soup.find(name="a", rel="noreferrer")
 # print(tag_find.text)
 
-article_link = article_tag.get('href')
-article_upvote = soup.find(name='span', class_="score").getText()
+# article_link = article_tag.get('href')
+# article_upvote = soup.find(name='span', class_="score").getText()
+#
+# print(article_link)
+# print(article_upvote)
+
+print('\n\n\n')
+all_article_tag = soup.find_all(name='a', rel="noreferrer")
+
+
+article_text = []
+article_link = []
+for tag in all_article_tag:
+    text = tag.getText()
+    article_text.append(text)
+    link = tag.get('href')
+    article_link.append(link)
+
+
+article_upvote = soup.find_all(name='span', class_="score")
+
 
 print(article_link)
-print(article_upvote)
+print(article_text)
