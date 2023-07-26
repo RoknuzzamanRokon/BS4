86377df46f5cfbdf5490364330dc52d9c69cb871
Rokon Ron <rokon.raz@gmail.com>
2023-07-26 14:17:51 +0600

    Get a title here.

--- live_website/main.py
+++ live_website/main.py
@@ -4,6 +4,6 @@ import requests
 response = requests.get("https://news.ycombinator.com/")
 lv_web_page = response.text
 
-all_tag = BeautifulSoup(lv_web_page, "html.parser")
+soup = BeautifulSoup(lv_web_page, "html.parser")
 
-print(all_tag)
+print(soup.title)
