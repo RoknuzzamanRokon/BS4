019d79a3660c2825317409e923d8a1b60c580a84
Rokon Ron <rokon.raz@gmail.com>
2023-07-26 14:55:55 +0600

    using getText method for taking a specific tag using specific class or rel.

--- live_website/main.py
+++ live_website/main.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://news.ycombinator.com/")
+response = requests.get("https://news.ycombinator.com/news")
 lv_web_page = response.text
 
 soup = BeautifulSoup(lv_web_page, "html.parser")
+# print(soup.find_all('a'))
+all_tag = soup.find(name='a', rel="noreferrer")
 
-print(soup.title)
+
+print(all_tag.getText())
