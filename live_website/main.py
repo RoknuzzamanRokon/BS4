ee4f3bb72ecc938ccd9aacf5e9f78e61f6401cf3
Rokon Ron <rokon.raz@gmail.com>
2023-07-26 16:52:49 +0600

    There find only one link or another point is collected.here also uesing findall method.

--- live_website/main.py
+++ live_website/main.py
@@ -5,11 +5,20 @@ response = requests.get("https://news.ycombinator.com/news")
 lv_web_page = response.text
 
 soup = BeautifulSoup(lv_web_page, "html.parser")
-all_tag_find = soup.find_all(name='a', rel="noreferrer")
+article_tag = soup.find(name='a', rel="noreferrer")
 
-for tag in all_tag_find:
-    all_tags_find_all = tag.getText()
-    print(all_tags_find_all)
+# for tag in all_tag_find:
+#     all_tags_find_all = tag.getText()
+#     print(all_tags_find_all)
 
 # all_tag = soup.find(name='a', rel="noreferrer")
 # print(all_tag.getText())
+#
+# tag_find = soup.find(name="a", rel="noreferrer")
+# print(tag_find.text)
+
+article_link = article_tag.get('href')
+article_upvote = soup.find(name='span', class_="score").getText()
+
+print(article_link)
+print(article_upvote)
